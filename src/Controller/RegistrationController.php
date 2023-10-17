56723db13d9b1d3f09f60e1ea516fbc5ad963d73
CharlesVazetta <charles.vanzetta@gmail.com>
2023-10-17 14:10:28 +0200

    registration controller php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\Participant;
-use App\Form\InscriptionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Participant;
+use App\Form\InscriptionType;
 
+/**
+ * @method getDoctrine()
+ */
 class RegistrationController extends AbstractController
 {
     /**
@@ -16,15 +19,15 @@ class RegistrationController extends AbstractController
      */
     public function registration(Request $request): Response
     {
-        $participant = new Participant(); // Créez une instance de votre entité Participant
+        $participant = new Participant();
         $form = $this->createForm(InscriptionType::class, $participant);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Traitez la soumission du formulaire, par exemple, en enregistrant l'utilisateur en base de données
-
-            // Redirigez l'utilisateur vers une autre page, par exemple, la page de connexion
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($participant);
+            $entityManager->flush();
         }
 
         return $this->render('registration/registration.html.twig', [
