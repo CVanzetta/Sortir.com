491f2c92baf897665157468c8046e5dc1dbb3e89
Kurai D <137479111+Kurai-D@users.noreply.github.com>
2023-10-20 09:57:23 +0200

    se souvenir de moi ajouté!

--- migrations/Version20231020073938.php
+++ migrations/Version20231020073938.php
@@ -21,7 +21,7 @@ final class Version20231020073938 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP INDEX UNIQ_D79F6B115126AC48 ON participant');
-        $this->addSql('ALTER TABLE participant CHANGE mail email VARCHAR(180) NOT NULL');
+        //$this->addSql('ALTER TABLE participant CHANGE mail email VARCHAR(180) NOT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D79F6B11E7927C74 ON participant (email)');
     }
 
@@ -29,7 +29,7 @@ final class Version20231020073938 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP INDEX UNIQ_D79F6B11E7927C74 ON participant');
-        $this->addSql('ALTER TABLE participant CHANGE email mail VARCHAR(180) NOT NULL');
+        //$this->addSql('ALTER TABLE participant CHANGE email mail VARCHAR(180) NOT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D79F6B115126AC48 ON participant (mail)');
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -11,12 +12,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: 'login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request,AuthenticationUtils $authenticationUtils): Response
     {
-
-        // get the login error if there is one
+          // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
+          // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
