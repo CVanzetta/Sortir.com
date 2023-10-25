722bd9cf774a23725726a75545d71886f052357f
QuentinNavarre <q.navarre@laposte.net>
2023-10-25 16:28:46 +0200

    Twig afficherProfil pour futur affichage utilisateur

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
 use App\Entity\Participant;
 use App\Form\EditProfilType;
+use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,7 +18,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class ProfilController extends AbstractController
 {
     #[Route('/monProfil', name:'profil_monProfil')]
-    public function editProfil(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, PasswordAuthenticatedUserInterface $user): Response
+    public function editProfil(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         /** @var Participant $participant */
         $participant = $this->getUser();
@@ -27,7 +29,7 @@ class ProfilController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $newPass = $form->get('newPass')->getData();
             if (null != $newPass) {
-                $participant->setMotPasse($passwordHasher->hashPassword($user, $newPass));
+                $participant->setMotPasse($passwordHasher->hashPassword($participant, $newPass));
 
                 /*$hashPass = $passwordHasher->hashPassword(
                     $user,
@@ -43,5 +45,17 @@ class ProfilController extends AbstractController
         return $this->render('profil/editProfil.html.twig', [
         'EditProfil' => $form->createView(),
     ]);
+    }
+
+    #[Route('/profil/{id}', name:'profil_afficherProfil')]
+    public function afficherProfil(Campus $campus, int $id, ParticipantRepository $participantRepository): Response
+    {
+        /** @var Participant $participant */
+        $participant = $participantRepository->find($id);
+
+        return $this->render('profil/afficherProfil.html.twig', [
+            "participant" => $participant,
+            "campus" => $campus
+        ]);
     }
 }
